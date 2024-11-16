9e2fc16c8ae166b4c5bca286921ffeb5bdbfd14f
Felipe <99846344+Felipe-Euzebio@users.noreply.github.com>
2024-11-15 22:21:35 -0300

    chore(backend/entity): add user properties

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @Column({ name: "Id"})
+  id: number;
+
+  @Column({ name: "Name" })
+  name: string;
+
+  @Column({ name: "Email" })
+  email: string;
+
+  @Column({ name: "Password" })
+  password: string;
+
+  @Column({ name: "Active" })
+  active: boolean;
+}
